23ec2065bc27e3622a4f9a225592e087652361f9
Taylor Monacelli <taylor.monacelli@streambox.com>
2016-06-29 09:07:52 -0700

    Backup and move win.ini out of way

--- uninstall.ps1
+++ uninstall.ps1
@@ -150,3 +150,25 @@ $timespan = new-timespan -minutes 10
 if (((get-date) - $lastWrite) -gt $timespan) {
    & $disable_proxy_block
 }
+
+# #############################
+# Remove win.ini
+# #############################
+$ini1 = "$env:windir\win.ini.4"
+if(test-path $ini1) { remove-item -force $ini1; }
+
+$ini1 = "$env:windir\win.ini.4"
+$ini2 = "$env:windir\win.ini.3"
+if(test-path $ini2) { rename-item $ini2 $ini1 }
+
+$ini1 = "$env:windir\win.ini.3"
+$ini2 = "$env:windir\win.ini.2"
+if(test-path $ini2) { rename-item $ini2 $ini1 }
+
+$ini1 = "$env:windir\win.ini.2"
+$ini2 = "$env:windir\win.ini.1"
+if(test-path $ini2) { rename-item $ini2 $ini1 }
+
+$ini1 = "$env:windir\win.ini.1"
+$ini2 = "$env:windir\win.ini"
+if(test-path $ini2) { rename-item $ini2 $ini1 }
